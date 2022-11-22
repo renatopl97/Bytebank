a3ab6beba59f8bef92da33ba55de44dba37dbe67
renatopl97 <renatopl97@gmail.com>
2022-11-21 23:09:17 -0300

    melhoria no código

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
     sacar(valor) {
-        if(this.saldo >= valor){
-            this.saldo -= valor;
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
         }
+    }
 
+    depositar(valor) {
+        if(valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
     }
 }
 
@@ -25,15 +32,15 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.sacar (200);
 
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+const valorSacado = contaCorrenteRicardo.sacar (50);
 
-console.log(contaCorrenteRicardo.saldo);
-console.log(cliente1);
-console.log(cliente2);
+console.log(valorSacado);
+console.log(contaCorrenteRicardo);
